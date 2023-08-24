80ff37a1af396d7c61878fae430839854c3ff4e8
tadeu <tadeusz.plesniak@gmail.com>
2023-08-24 16:41:02 +0200

    It's not over i guess

--- r_peaks_calc_save.py
+++ r_peaks_calc_save.py
@@ -1,9 +1,11 @@
 import csv
 import os
+from functools import reduce
 
 import matplotlib.pyplot as plt
 import neurokit2 as nk2
 import numpy as np
+from scipy.signal import convolve
 
 import MyEDFImports as m
 
@@ -16,7 +18,7 @@ def load_data_labels(names):
     for edf_filename in names:
         edf_labels = m.load_labels_one_file(edf_filename)
         raw = m.import_ecg(edf_filename)
-        data = raw[0][0].T
+        data = raw[0][0].flatten()
         raw_data_all.append(data)
         labels_all.append(edf_labels)
 
@@ -24,6 +26,7 @@ def load_data_labels(names):
 
 
 def make_windows(data_list, labels_list, win_pad):
+    """win_pad -> number of windows taken from left and right to the labeled window"""
     assert len(data_list) == len(labels_list)
     assert win_pad > 0 and type(win_pad) == int
     win_len = (2 * win_pad + 1) * 500 * 20
@@ -93,20 +96,33 @@ def save_to_csv(windows_r_peaks_stream):
     for f in open_files.values():
         f.close()
 
+
 def load_r_peaks_csv(dir):
     files = os.listdir(dir)
     peaks = {}
     for file in files:
         lists = []
         if file[-4:] == '.csv':
-            f_name = dir +f'/{file}'
+            f_name = dir + f'/{file}'
             with open(f_name) as f:
                 reader = csv.reader(f)
                 for row in reader:
-                    print(row)
                     lists.append([int(value) for value in row])
         peaks[int(file[:-4])] = lists
     return peaks
+
+
+def get_variance(data, nr_seconds):
+    win_len = int(nr_seconds * 500)
+    var_win = np.ones(win_len) / win_len
+    mov_avg = convolve(data, var_win, mode='same')
+    variance = convolve(data ** 2, var_win, mode='same') - mov_avg ** 2
+    return variance
+
+def variance_over_threshold(windowed_stream, nr_seconds, threshold):
+    for window, label in windowed_stream:
+        var = get_variance(window, nr_seconds)
+        yield window, label, np.any(var >= threshold)
 
 
 if __name__ == '__main__':
@@ -114,20 +130,35 @@ if __name__ == '__main__':
     # get data for every patient in a list of np arrays
     data, labels_init = load_data_labels(edf_filenames)
     # In[]
-    windows_stream = make_windows(data, labels_init, win_pad=2)
-    windows_r_peaks_stream = calc_r_peaks(windows_stream)
-
-    del_from_dir('r_peaks')
-    save_to_csv(windows_r_peaks_stream)
-
-    loaded = load_r_peaks_csv('r_peaks')
-
-
+    windowed_stream = make_windows(data, labels_init, 2)
+    win_over_thresh = variance_over_threshold(windowed_stream, 1, threshold=1.5e-6)
+    bad_ones = []
+    for x in win_over_thresh:
+        if x[2]:
+            bad_ones.append(x)
 # In[]
-    _, labels = zip(*make_windows(data, labels_init, 2))
-    from collections import Counter
-    Counter(labels)
-    for label, peaks in loaded.items():
-        print(label, len(peaks))
+    bad_ind = 35
+    plt.plot(bad_ones[bad_ind][0])
+    plt.show()
+
+    # print(reduce(lambda x, init: init + int(x[2]), win_over_thresh), 0)
+    ex_data = data[0]
+    variance_first_file = get_variance(ex_data, 1)
+    where_big_var = np.where(variance_first_file > 1.5e-6)
+    # In[]
+
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(variance_first_file)
+    plt.show()
+    # In[]
+    ex_big_var_ind = np.random.choice(where_big_var[0])
+    ex_good_sec = ex_data[10000:10500]
+    ex_one_sec = ex_data[ex_big_var_ind - 250:ex_big_var_ind + 250]
+    plt.plot(ex_one_sec)
+    plt.title(f'apporx seconds: {ex_big_var_ind}: variance: {variance_first_file[ex_big_var_ind]}')
+    plt.plot(ex_good_sec)
+    plt.show()
 # In[]
+
 
